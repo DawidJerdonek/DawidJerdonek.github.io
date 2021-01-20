cebe70530f3c346d584c62d007d3fed341f6f214
David Jerdonek <dawid.jerdonek@gmail.com>
2021-01-20 10:37:51 +0000

    Completed pop up

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@
 // including functions
 // Author Dawid Jerdonek
 
-var result = url.split("="); // Splits string based on =
+//var href = window.location.href
+
+//href will be equal to "https://DawidJerdonek.github.io/form/index.html?enemy=boss"
+//console.log(href);
+//var protocol = window.location.protocol
+//protocol will be equal to "https:"
+
+//var host = window.location.host
+//host will be equal to "DawidJerdonek.github.io"
+
+//var pathname = window.location.pathname
+//pathname will be equal to “/form/index.html”
+
+//var search = window.location.search 
+//search will be equal to “?enemy=boss"
 
 // get a handle to the canvas context
 var canvas = document.getElementById("game");
@@ -302,6 +316,7 @@ function onPageLoad()
     var url = document.location.href;
     var search = window.location.search;
 	
+	var result = url.split("="); // Splits string based on =
 	//alert(url);
     //alert(search);
 	//alert(result[0]);
